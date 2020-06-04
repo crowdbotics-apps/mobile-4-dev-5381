a5f9bd79e5ac4a34cd35d6ce57cf9670d4e5eec7
Crowdbotics <team@crowdbotics.com>
2020-06-04 07:46:51 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -8,6 +8,32 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
+    sasfsdf = models.OneToOneField(
+        "home.Test",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="customtext_sasfsdf",
+    )
+    dfsasa = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="customtext_dfsasa",
+    )
+    sadfasf = models.ManyToManyField(
+        "home.Fnhgjhjgjh", blank=True, related_name="customtext_sadfasf",
+    )
+    sdfdsf = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="customtext_sdfdsf",
+    )
+    fdgg = models.BigIntegerField(null=True, blank=True,)
+    sdfsdf = models.BigIntegerField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
